CTSRD-CHERI/llvm-project

commit a131c69cc20755a0089b2396c9de467e3d57354e

[CHERI] Implement cap relocs with CBuildCap strategy

lld/ELF/Arch/Cheri.cpp
@@ -5,9 +5,16 @@
 #include "../SyntheticSections.h"
 #include "../Target.h"
 #include "../Writer.h"
+#include "Relocations.h"
 #include "lld/Common/CommonLinkerContext.h"
+#include "lld/Common/ErrorHandler.h"
 #include "lld/Common/Memory.h"
+#include "llvm/ADT/APInt.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/BinaryFormat/ELF.h"
 #include "llvm/Support/Path.h"
+#include <cmath>
+#include <cstdint>
 
 using namespace llvm;
 using namespace llvm::object;
@@ -20,6 +27,178 @@ using namespace llvm::ELF;
 
 namespace lld {
 namespace elf {
+
+enum PermissionKind {
+  PK_FUNC = 0,
+  PK_OBJ,
+  PK_CONST,
+};
+
+enum ArchPermTy {
+  AP_CAP = 0,
+  AP_WRITE,
+  AP_READ,
+  AP_EXEC,
+  AP_ASR,
+};
+
+constexpr uint64_t bit(size_t n) { return 1UL << n;}
+
+template <class T> constexpr T ones(size_t bits) { return (1U << bits) - 1; }
+
+template <class T> constexpr T truncate(T val, size_t bits) {
+  static_assert(std::is_unsigned_v<T>, "Only use unsigned values!");
+  return val & ones<T>(bits);
+}
+
+template <class T> constexpr unsigned getBit(T val, size_t bit) {
+  return ((1 << (bit)) & val) == 0 ? 0U : 1U;
+}
+
+// Architectural Permissions
+struct AP {
+  static uint64_t function() {
+    if (config->is64) {
+      return (bit(AP_CAP) | bit(AP_READ) | bit(AP_EXEC) | bit(AP_ASR)) << 47;
+    } else {
+      // Quandrant 1 - value 2
+      return (0b01010) << 25;
+    }
+  }
+  static uint64_t readOnly() {
+    if (config->is64) {
+      return (bit(AP_CAP) | bit(AP_READ) | bit(AP_ASR)) << 47;
+    } else {
+      // Quandrant 3 - value 3
+      return (0b11011) << 25;
+    }
+  }
+  static uint64_t readWrite() {
+    if (config->is64) {
+      return (bit(AP_CAP) | bit(AP_WRITE) | bit(AP_READ) | bit(AP_ASR)) << 47;
+    } else {
+      // Quandrant 3 - value 7
+      return (0b11111) << 25;
+    }
+  }
+};
+
+PermissionKind getCapabilityPermissionKind(const Symbol &sym) {
+  PermissionKind PK = PK_OBJ;
+  if (sym.isFunc())
+    PK = PK_FUNC;
+  else if (auto *os = sym.getOutputSection()) {
+    if ((os->flags & SHF_WRITE) == 0 || isRelroSection(os)) {
+      PK = PK_CONST;
+    } else {
+      PK = PK_OBJ;
+    }
+    if (os->flags & SHF_EXECINSTR) {
+      warn("Non-function __cap_reloc against symbol in section with "
+           "SHF_EXECINSTR (" +
+           toString(os->name) + ") for symbol " + toString(sym));
+    }
+  }
+  return PK;
+}
+
+uint64_t encodeCapabilityPermissions(const Symbol &sym) {
+  switch (getCapabilityPermissionKind(sym)) {
+  case PK_FUNC:
+    return AP::function();
+  case PK_OBJ:
+    return AP::readWrite();
+  case PK_CONST:
+    return AP::readOnly();
+  }
+  return 0;
+}
+
+// Morello style capability fragment.
+// [Size [XLEN-1:5], Permissions [4:0]]
+uint64_t encodeAlternativeMeta(const Symbol &sym, uint64_t l) {
+  return (l << 5) | getCapabilityPermissionKind(sym);
+}
+
+template <class ELFT>
+uint64_t encodeCapabilityBounds(uint64_t b, uint64_t l, bool *exact) {
+  using NativeTy = std::conditional_t<ELFT::Is64Bits, uint64_t, uint32_t>;
+  const uint64_t MXLEN = ELFT::Is64Bits ? 64 : 32;
+  const uint64_t MW = ELFT::Is64Bits ? 14 : 10;
+  const uint64_t EW = ELFT::Is64Bits ? 6 : 5;
+  const uint64_t EF_BIT_POS = ELFT::Is64Bits ? 26 : 19;
+  const uint64_t ADDR_BW = ELFT::Is64Bits ? 65 : 33;
+  const uint64_t IE_WIDTH = MW - (EW / 2);
+  const uint64_t EXP_PART_SIZE = EW / 2;
+  const uint64_t CAP_MAX_E = MXLEN - MW + 2;
+
+  const uint64_t t = b + l;
+  assert(t - b == l && "Length too large - Overflow happened!");
+  uint64_t E =  CAP_MAX_E -
+               countl_zero(static_cast<NativeTy>(l | ones<NativeTy>(MW - 1))) - 1;
+  assert(E <= CAP_MAX_E && "Exponent too large!");
+  const bool IE = E != 0 || getBit(l, MW - 2) == 1;
+  const uint64_t EF = IE ? 0 : 1;
+  uint64_t L8 = 0;
+
+  uint64_t T = 0;
+  uint64_t B = 0;
+  if (!IE) {
+    B = truncate(b, MW);
+    T = truncate(t, MW);
+    if (!ELFT::Is64Bits)
+      L8 = getBit(l, 8);
+    if (exact)
+      *exact = true;
+  } else {
+    uint64_t B_ie = truncate(b >> (E + 3), IE_WIDTH);
+    uint64_t T_ie = truncate(t >> (E + 3), IE_WIDTH);
+
+    APInt maskLo = (APInt(ADDR_BW, 1) << (E + 3)) - 1;
+    APInt zero65 = APInt(ADDR_BW, 0);
+    bool lostSignificantBase = (APInt(ADDR_BW, b) & maskLo) != zero65;
+    bool lostSignificantTop = (APInt(ADDR_BW, t) & maskLo) != zero65;
+
+    if (lostSignificantTop) {
+      T_ie = truncate(T_ie + 1, IE_WIDTH);
+    }
+
+    uint64_t l_ie = truncate(T_ie - B_ie, IE_WIDTH);
+    bool overflowed = getBit(l_ie, IE_WIDTH - 1);
+    bool incE = false;
+    if (overflowed) {
+      incE = true;
+      lostSignificantBase |= getBit(B_ie, 0) == 1;
+      lostSignificantTop |= getBit(T_ie, 0) == 1;
+
+      B_ie = truncate(b >> (E + 3 + 1), IE_WIDTH);
+      T_ie = truncate(t >> (E + 3 + 1), IE_WIDTH);
+      if (lostSignificantTop)
+        T_ie = truncate(T_ie + 1, IE_WIDTH);
+    }
+    E += incE ? 1 : 0;
+
+    // sort out the exponent
+    E = CAP_MAX_E - E;
+    const uint64_t E_B = truncate(E, EXP_PART_SIZE);
+    const uint64_t E_T = truncate(E >> EXP_PART_SIZE, EXP_PART_SIZE);
+    if (!ELFT::Is64Bits)
+      L8 = getBit(E, EW - 1);
+    B = (B_ie << EXP_PART_SIZE) | E_B;
+    T = (T_ie << EXP_PART_SIZE) | E_T;
+
+    if (exact)
+      *exact = !(lostSignificantBase || lostSignificantTop);
+  }
+
+  uint64_t Bits = 0;
+  Bits |= truncate(B, MW);
+  Bits |= truncate(T, MW - 2) << MW;
+  Bits |= EF << EF_BIT_POS;
+  if (!ELFT::Is64Bits)
+    Bits |= (L8 << 18);
+  return Bits;
+}
 
 // See CheriBSD crt_init_globals()
 template <class ELFT> struct InMemoryCapRelocEntry {
@@ -1085,6 +1264,16 @@ void CheriCapTableMappingSection::writeTo(uint8_t *buf) {
   memcpy(buf, entries.data(), entries.size() * sizeof(CaptableMappingEntry));
 }
 
+template <class ELFT>
+void writeCatableRelocationFragments(InputSectionBase *sec, Symbol *sym,
+                                     uint64_t offset) {
+  const uint64_t wordSize = ELFT::Is64Bits ? 8 : 4;
+  sec->addReloc(
+      {R_CHERI_CAPTAB_FRAG_ADDR, target->symbolicRel, offset, 0, sym});
+  sec->addReloc({R_CHERI_CAPTAB_FRAG_META, target->symbolicRel,
+                 offset + wordSize, 0, sym});
+}
+
 template <typename ELFT>
 void addCapabilityRelocation(Symbol *sym, RelType type, InputSectionBase *sec,
                              uint64_t offset, RelExpr expr, int64_t addend,
@@ -1202,8 +1391,37 @@ void addCapabilityRelocation(Symbol *sym, RelType type, InputSectionBase *sec,
                                     sym->isPreemptible, addend);
   } else {
     assert(config->localCapRelocsMode == CapRelocsMode::CBuildCap);
-    error("CBuildCap method not implemented yet!");
+    assert(!sym->includeInDynsym() && "Must not be a dynamic symbol");
+    if (config->emachine != EM_RISCV)
+      error("CBuildCap method not implemented yet!");
+    in.relaDyn->addReloc({R_RISCV_CHERI_RELATIVE, sec, offset,
+                          DynamicReloc::AgainstSymbol, *sym, addend, R_ABS});
+    writeCatableRelocationFragments<ELFT>(sec, sym, offset);
   }
+}
+
+uint64_t getCapMetaBits(int64_t a, const Symbol &sym,
+                        const InputSectionBase *isec, uint64_t offset) {
+  const uint64_t baseAddr = sym.getVA(a);
+  const uint64_t shift = config->is64 ? 8 : 4;
+  CheriCapRelocLocation loc{const_cast<InputSectionBase *>(isec),
+                            offset - shift};
+  CheriCapReloc reloc{SymbolAndOffset{const_cast<Symbol *>(&sym), 0}, 0,
+                      static_cast<bool>(sym.isPreemptible)};
+
+  bool exact = false;
+  uint64_t symSize = invokeAndRetELFT(getTargetSize, loc, reloc);
+  uint64_t metaBits =
+      invokeAndRetELFT(encodeCapabilityBounds, baseAddr, symSize, &exact);
+
+  if (!exact)
+    nonFatalWarning(
+        "Capability written to captable with rounding. Base Address = 0x" +
+        utohexstr(baseAddr) + ", size=0x" + utohexstr(symSize) +
+        ", the bits writter are 0x" + utohexstr(metaBits));
+
+  // TODO - write out raw capability bits instead of adhoc format.
+  return encodeAlternativeMeta(sym, symSize);
 }
 
 } // namespace elf

clang/lib/Headers/cheri_init_globals_bw.h
@@ -23,6 +23,15 @@ struct capreloc {
   __SIZE_TYPE__ permissions;
 };
 
+struct ELFRela {
+  __SIZE_TYPE__ offset;
+  __SIZE_TYPE__ info;
+  __SIZE_TYPE__ addend;
+};
+
+// Bit of a hack as we should include it from llvm
+#define R_RISCV_CHERI_RELATIVE 202
+
 #define GET_BIT_MASK(OFF_BITS) ~(__SIZE_TYPE__)(OFF_BITS)
 
 static const __SIZE_TYPE__ function_reloc_flag = (__SIZE_TYPE__)1
@@ -99,6 +108,96 @@ static __attribute__((always_inline)) void cheri_init_globals_impl(
 }
 
 static __attribute__((always_inline)) void
+cheri_init_globals_cbuildcap(void *__capability rw, void *__capability rx) {
+  const struct ELFRela *start_rela;
+  const struct ELFRela *stop_rela;
+  __SIZE_TYPE__ start_addr, stop_addr;
+#if !defined(__CHERI_PURE_CAPABILITY__)
+  __asm__("lla %0, __rela_dyn_start\n\t"
+          "lla %1, __rela_dyn_end\n\t"
+          : "=r"(start_addr), "=r"(stop_addr));
+#else
+  void *__capability tmp;
+  __asm__("llc %2, __rela_dyn_start\n\t"
+          "cgetaddr %0, %2\n\t"
+          "llc %2, __rela_dyn_end\n\t"
+          "cgetaddr %1, %2\n\t"
+          : "=r"(start_addr), "=r"(stop_addr), "=&C"(tmp));
+#endif
+  if (start_addr == 0 || stop_addr == 0)
+    return;
+
+#if !defined(__CHERI_PURE_CAPABILITY__)
+  start_rela = (const struct ELFRela *)(__UINTPTR_TYPE__)start_addr;
+  stop_rela = (const struct ELFRela *)(__UINTPTR_TYPE__)stop_addr;
+#else
+  __SIZE_TYPE__ rela_size = stop_addr - start_addr;
+  start_rela = (const struct ELFRela *)__builtin_cheri_address_set(
+      __builtin_cheri_program_counter_get(), start_addr);
+  start_rela = __builtin_cheri_bounds_set(start_rela, rela_size);
+  stop_rela = (const struct ELFRela *)(const void *)((const char *)start_rela +
+                                                    rela_size);
+#endif
+
+#if !defined(__CHERI_PURE_CAPABILITY__) || __CHERI_CAPABILITY_TABLE__ == 3
+    /* pc-relative or hybrid ABI -> need large bounds on $pcc */
+    bool can_set_code_bounds = false;
+#else
+    bool can_set_code_bounds = true; /* fn-desc/plt ABI -> tight bounds okay */
+#endif
+
+  struct CapFrag {
+    __SIZE_TYPE__ addr;
+    __SIZE_TYPE__ meta;
+  };
+
+  for (const struct ELFRela *r = start_rela; r < stop_rela; r++) {
+    if (r->info != R_RISCV_CHERI_RELATIVE)
+      continue;
+
+    void *__capability *__capability dest =
+        (void *__capability *__capability)__builtin_cheri_address_set(
+            rw, r->offset);
+    struct CapFrag *__capability frag = (struct CapFrag *__capability)(dest);
+
+    if (frag->addr == 0) {
+      *dest = (void *__capability)0;
+      continue;
+    }
+
+    __SIZE_TYPE__ length = frag->meta >> 5;
+    __SIZE_TYPE__ perms = frag->meta & ((1 << 5) - 1);
+    enum PermKind {
+      PK_FUNC = 0,
+      PK_OBJ,
+      PK_CONST,
+    };
+
+    bool is_func = perms == PK_FUNC;
+    bool can_set_bounds = (!is_func || can_set_code_bounds) && length != 0;
+    void *auth_cap = is_func ? rx : rw;
+    if (perms == PK_FUNC) {
+      auth_cap = __builtin_cheri_perms_and(auth_cap,
+                                           function_pointer_permissions_mask);
+    } else if (perms == PK_OBJ) {
+      auth_cap =
+          __builtin_cheri_perms_and(auth_cap, global_pointer_permissions_mask);
+    } else if (perms == PK_CONST) {
+      auth_cap = __builtin_cheri_perms_and(auth_cap,
+                                           constant_pointer_permissions_mask);
+    }
+
+    void *cap = __builtin_cheri_address_set(auth_cap, frag->addr);
+    if (can_set_bounds)
+      cap = __builtin_cheri_bounds_set(cap, length);
+    cap = __builtin_cheri_offset_increment(cap, r->addend);
+    if (is_func)
+      cap = __builtin_cheri_seal_entry(cap);
+    *dest = cap;
+  }
+}
+
+static __attribute__((always_inline)) void
 cheri_init_globals_3(void *__capability data_cap,
                      const void *__capability code_cap,
                      const void *__capability rodata_cap) {
@@ -117,6 +216,9 @@ cheri_init_globals_3(void *__capability data_cap,
           "cgetaddr %1, %2\n\t"
           : "=r"(start_addr), "=r"(stop_addr), "=&C"(tmp));
 #endif
+
+  if (start_addr == 0 || stop_addr == 0)
+    return;
 
 #if !defined(__CHERI_PURE_CAPABILITY__)
   start_relocs = (const struct capreloc *)(__UINTPTR_TYPE__)start_addr;

clang/test/CodeGenCXX/cheri/cheri_init_globals_bw.cpp
@@ -14,5 +14,6 @@ void _start(void);
 
 
 void _start(void) {
-  cheri_init_globals();
+  cheri_init_globals_cbuildcap(__builtin_cheri_global_data_get(),
+                               __builtin_cheri_program_counter_get());
 }

lld/ELF/Arch/Cheri.h
@@ -325,6 +325,9 @@ void addCapabilityRelocation(Symbol *sym, RelType type, InputSectionBase *sec,
                              bool isCallExpr,
                              llvm::function_ref<std::string()> referencedBy,
                              RelocationBaseSection *dynRelSec = nullptr);
+
+uint64_t getCapMetaBits(int64_t a, const Symbol &sym,
+                        const InputSectionBase *isec, uint64_t offset);
 } // namespace elf
 } // namespace lld
 

lld/ELF/Arch/RISCV.cpp
@@ -46,6 +46,7 @@ public:
   void relocate(uint8_t *loc, const Relocation &rel,
                 uint64_t val) const override;
   bool relaxOnce(int pass) const override;
+  void relocateAlloc(InputSectionBase &sec, uint8_t *buf) const override;
 };
 
 } // end anonymous namespace
@@ -848,6 +849,34 @@ bool RISCV::relaxOnce(int pass) const {
       changed |= relax(*sec);
   }
   return changed;
+}
+
+void RISCV::relocateAlloc(InputSectionBase &sec, uint8_t *buf) const {
+  const unsigned bits = config->is64 ? 64 : 32;
+  uint64_t secAddr = sec.getOutputSection()->addr;
+  if (auto *s = dyn_cast<InputSection>(&sec))
+    secAddr += s->outSecOff;
+  for (const Relocation &rel : sec.relocs()) {
+    uint8_t *loc = buf + rel.offset;
+    const uint64_t val =
+        SignExtend64(sec.getRelocTargetVA(sec.file, rel.type, rel.addend,
+                                          secAddr + rel.offset, *rel.sym,
+                                          rel.expr, &sec, rel.offset),
+                     bits);
+    switch (rel.expr) {
+    case lld::elf::R_CHERI_CAPTAB_FRAG_ADDR:
+    case lld::elf::R_CHERI_CAPTAB_FRAG_META:
+      if(bits == 64)
+          write64(loc, val);
+      else
+          write32(loc, val);
+      continue;
+    default:
+      break;
+    }
+    if (rel.expr != R_RELAX_HINT)
+      relocate(loc, rel, val);
+  }
 }
 
 void elf::riscvFinalizeRelax(int passes) {

lld/ELF/Driver.cpp
@@ -431,7 +431,8 @@ static void checkOptions() {
   }
   if (config->localCapRelocsMode == CapRelocsMode::ElfReloc)
     error("local-cap-relocs=elf is not implemented yet");
-  if (config->localCapRelocsMode == CapRelocsMode::CBuildCap)
+  if (config->localCapRelocsMode == CapRelocsMode::CBuildCap &&
+      config->emachine != EM_RISCV)
     error("local-cap-relocs=cbuildcap is not implemented yet");
   assert(config->preemptibleCapRelocsMode != CapRelocsMode::CBuildCap);
 

lld/ELF/InputSection.cpp
@@ -901,6 +901,10 @@ uint64_t InputSectionBase::getRelocTargetVA(const InputFile *file, RelType type,
   case R_MIPS_CHERI_CAPTAB_TPREL:
     assert(a == 0 && "capability table index relocs should not have addends");
     return in.cheriCapTable->getTlsOffset(sym);
+  case R_CHERI_CAPTAB_FRAG_ADDR:
+    return sym.getVA(a);
+  case R_CHERI_CAPTAB_FRAG_META:
+    return getCapMetaBits(a, sym, isec, offset);
   default:
     llvm_unreachable("invalid expression");
   }

lld/ELF/Relocations.h
@@ -113,6 +113,8 @@ enum RelExpr {
   R_MIPS_CHERI_CAPTAB_TLSGD,
   R_MIPS_CHERI_CAPTAB_TLSLD,
   R_MIPS_CHERI_CAPTAB_TPREL,
+  R_CHERI_CAPTAB_FRAG_ADDR,
+  R_CHERI_CAPTAB_FRAG_META,
   R_CHERI_CAPABILITY,
   // Same as R_PC but with page-aligned semantics.
   R_LOONGARCH_PAGE_PC,

jrtc27/cheri-alliance-llvm-project

commit 55f89ec43d854e47dd388adcd7f5be4a9711d29e

[ELF][RISCV][CHERI] Never use symbolic relocation for R_RISCV_CHERI_RELATIVE
Currently this "works" because LLD is lax and throws away the symbol
when not present in a dynamic symbol table (either because it's not
exported or because there is no dynamic symbol table at all) and so it
behaves like AddendOnlyWithTargetVA. However, there are cases where we
create relative relocations against symbols where this is not the case,
such as --export-dynamic for a dynamic executable, -Bsymbolic for a DSO
referencing an exported symbol, or a DSO referencing a protected symbol.
In these cases, this implementation ends up incorrectly emitting an
object with a non-zero symbol index for this relative relocation.

This was likely copied from Morello LLVM that used to have this same bug
prior to 0c3e5e819665 ("[ELF][Morello] Reimplement fragment addend
calculation as a RelExpr").

Fixes: a131c69cc207 ("[CHERI] Implement cap relocs with CBuildCap strategy")

lld/ELF/Arch/Cheri.cpp
@@ -1055,8 +1055,9 @@ void addRelativeCapabilityRelocation(
       error("Relative Relocs method not implemented yet!");
     RelocationBaseSection &oSec =
         sym->includeInDynsym(ctx) ? *ctx.mainPart->relaDyn : *ctx.in.relaDyn;
-    oSec.addReloc(DynamicReloc::AgainstSymbol, R_RISCV_CHERI_RELATIVE, isec,
-                  offsetInSec, *sym, addend, expr, ctx.target->symbolicRel);
+    oSec.addReloc(DynamicReloc::AddendOnlyWithTargetVA, R_RISCV_CHERI_RELATIVE,
+                  isec, offsetInSec, *sym, addend, expr,
+                  ctx.target->symbolicRel);
     writeCatableRelocationFragments(ctx, &isec, sym, offsetInSec);
     return;
   }