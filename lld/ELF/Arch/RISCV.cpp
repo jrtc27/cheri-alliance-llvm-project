CTSRD-CHERI/llvm-project

commit 93ffe60b89895744f01099684508c05d1b3c482a

[CHERI] Support Cheri RISCV Lazy Binding

lld/ELF/Arch/RISCV.cpp
@@ -17,6 +17,7 @@
 #include "llvm/Support/RISCVAttributes.h"
 #include "llvm/Support/RISCVISAInfo.h"
 #include "llvm/Support/TimeProfiler.h"
+#include "Cheri.h"
 
 using namespace llvm;
 using namespace llvm::object;
@@ -66,7 +67,7 @@ enum Op {
   SRLI = 0x5013,
   SUB = 0x40000033,
 
-  CIncOffsetImm = 0x201B,
+  CADDI = 0x201B,
   CLC = 0x400F,
 };
 
@@ -136,6 +137,7 @@ RISCV::RISCV() {
 
   // .got.plt[0] = _dl_runtime_resolve, .got.plt[1] = link_map
   gotPltHeaderEntriesNum = 2;
+  //cheriCapTableHeaderEntriesNum = 2;
 
   pltHeaderSize = 32;
   pltEntrySize = 16;
@@ -256,7 +258,7 @@ void RISCV::writePltHeader(uint8_t *buf) const {
   // (if shift == 0): nop
   uint32_t offset = in.gotPlt->getVA() - in.plt->getVA();
   uint32_t ptrload = config->isCheriAbi ? CLC : config->is64 ? LD : LW;
-  uint32_t ptraddi = config->isCheriAbi ? CIncOffsetImm : ADDI;
+  uint32_t ptraddi = config->isCheriAbi ? CADDI : ADDI;
   // Shift is log2(pltsize / ptrsize), which is 0 for CHERI-128 so skipped
   uint32_t shift = 2 - config->is64 - config->isCheriAbi;
   uint32_t ptrsize = config->isCheriAbi ? config->capabilitySize

lld/ELF/Arch/Cheri.cpp
@@ -906,6 +906,9 @@ uint64_t MipsCheriCapTableSection::assignIndices(uint64_t startIndex,
 
     uint32_t index = *cti.index;
     assert(index >= startIndex && index < startIndex + entries.size());
+    if (in.plt->isNeeded()) {
+      assert(index >= target->cheriCapTableHeaderEntriesNum);
+    }
     Symbol *targetSym = it.first;
 
     StringRef name = targetSym->getName();
@@ -974,7 +977,9 @@ uint64_t MipsCheriCapTableSection::assignIndices(uint64_t startIndex,
 template <class ELFT>
 void MipsCheriCapTableSection::assignValuesAndAddCapTableSymbols() {
   // First assign the global indices (which will usually be the only ones)
-  uint64_t assignedEntries = assignIndices<ELFT>(0, globalEntries, "");
+  uint64_t assignedEntries =
+      partitionGlobalEntries() ? target->cheriCapTableHeaderEntriesNum : 0;
+  assignedEntries += assignIndices<ELFT>(assignedEntries, globalEntries, "");
   if (LLVM_UNLIKELY(config->capTableScope != CapTableScopePolicy::All)) {
     assert(assignedEntries == 0 && "Should not have any global entries in"
                                    " per-file/per-function captable mode");
@@ -1057,6 +1062,32 @@ void MipsCheriCapTableSection::assignValuesAndAddCapTableSymbols() {
   }
 
   valuesAssigned = true;
+}
+
+bool MipsCheriCapTableSection::partitionGlobalEntries() {
+  assert(!valuesAssigned && "Values should not be assigned yet");
+  bool containsPltSymbols = false;
+  if (config->emachine != EM_RISCV)
+    return containsPltSymbols;
+  CaptableMap globalMapCopy = globalEntries;
+  globalEntries.map.clear();
+  for (const auto &it : globalMapCopy.map) {
+    if (!it.first->isInPlt())
+      continue;
+    auto inserted = globalEntries.map.insert(it);
+    (void)inserted;
+    assert(inserted.second && "Entry should not have already been inserted.");
+    containsPltSymbols = true;
+  }
+
+  for (const auto &it : globalMapCopy.map) {
+    if (it.first->isInPlt())
+      continue;
+    auto inserted = globalEntries.map.insert(it);
+    (void)inserted;
+    assert(inserted.second && "Entry should not have already been inserted.");
+  }
+  return containsPltSymbols;
 }
 
 template void

lld/ELF/Arch/Cheri.h
@@ -200,8 +200,10 @@ public:
   uint32_t getTlsIndexOffset() const;
   uint32_t getTlsOffset(const Symbol &sym) const;
   bool isNeeded() const override {
-    return nonTlsEntryCount() != 0 || !dynTlsEntries.empty() ||
-           !tlsEntries.empty();
+    return nonTlsEntryCount() > (in.plt->isNeeded()
+                                     ? target->cheriCapTableHeaderEntriesNum
+                                     : 0) ||
+           !dynTlsEntries.empty() || !tlsEntries.empty();
   }
   void writeTo(uint8_t *buf) override;
   template <class ELFT> void assignValuesAndAddCapTableSymbols();
@@ -245,6 +247,8 @@ private:
                                               uint64_t offset);
   size_t nonTlsEntryCount() const {
     size_t totalCount = globalEntries.size();
+    if(totalCount > 0 && in.plt->isNeeded())
+      totalCount += target->cheriCapTableHeaderEntriesNum;
     if (LLVM_LIKELY(config->capTableScope == CapTableScopePolicy::All)) {
       assert(perFileEntries.empty() && perFunctionEntries.empty());
     } else {
@@ -256,6 +260,8 @@ private:
     }
     return totalCount;
   }
+
+  bool partitionGlobalEntries();
 
   // The two maps are only used in the experimental
   llvm::MapVector<InputFile *, CaptableMap> perFileEntries;

lld/ELF/Target.h
@@ -143,6 +143,8 @@ public:
   // to support lazy loading.
   unsigned gotPltHeaderEntriesNum = 3;
 
+  unsigned cheriCapTableHeaderEntriesNum = 0;
+
   // On PPC ELF V2 abi, the first entry in the .got is the .TOC.
   unsigned gotHeaderEntriesNum = 0;
 

lld/test/ELF/cheri/__cap_relocs/cbuildcap-large.s
@@ -3,18 +3,28 @@
 # RUN: ld.lld --local-caprelocs=cbuildcap %t.rv32.o -o %t.rv32
 
 # RUN: llvm-readobj --relocs %t.rv32 | FileCheck %s --check-prefix=RELOCS
+# RUN: llvm-objdump -s --section=.rela.dyn %t.rv32 | FileCheck %s --check-prefix=RELADYN
 # RUN: llvm-objdump -s --section=.got %t.rv32 | FileCheck %s --check-prefix=GOT
+# RUN: llvm-readelf -s %t.rv32 | FileCheck %s --check-prefix=SYM
 
 # RELOCS:      Relocations [
 # RELOCS-NEXT:   Section ({{[0-9]+}}) .rela.dyn {
 # RELOCS-NEXT:     0x12250 R_RISCV_CHERI_RELATIVE - 0x0
 # RELOCS-NEXT:   }
 
+#RELADYN: Contents of section .rela.dyn:
+#RELADYN-NEXT: 10200 
+#RELADYN-NEXT: 10210 00000000 00000000
+
 # GOT: Contents of section .got:
 # GOT-NEXT: 12240
 # GOT-NEXT: 12250 60320100 00000000 33994f02 0070e201
-#                    [    address    ] [      meta     ]
+#                 [    address    ] [      meta     ]
+#                 address = 0x13250 -> matches symbol VA
 
+# SYM:      0000000000010200    24 NOTYPE  LOCAL  HIDDEN      1 __rela_dyn_start
+# SYM-NEXT: 0000000000010218     0 NOTYPE  LOCAL  HIDDEN      1 __rela_dyn_end
+# SYM:      0000000000013250  8193 OBJECT  GLOBAL DEFAULT     4 x
         .text
         .attribute      4, 16
         .attribute      5, "rv64i2p1_zcheripurecap0p9"
@@ -53,4 +63,8 @@ x:
 .global _start 
 _start:
    call foo 
+.weak __rela_dyn_start
+.hidden __rela_dyn_start
+.weak __rela_dyn_end
+.hidden __rela_dyn_end
 

lld/test/ELF/cheri/__cap_relocs/cbuildcap.s
@@ -3,21 +3,27 @@
 # RUN: ld.lld --local-caprelocs=cbuildcap %t.rv32.o -o %t.rv32
 
 # RUN: llvm-readobj --relocs %t.rv32 | FileCheck %s --check-prefix=RELOCS
+# RUN: llvm-objdump -s --section=.rela.dyn %t.rv32 | FileCheck %s --check-prefix=RELADYN
 # RUN: llvm-objdump -s --section=.got %t.rv32 | FileCheck %s --check-prefix=GOT
-# RUN: llvm-readobj --symbols %t.rv32 | FileCheck %s --check-prefix=SYM
+# RUN: llvm-readelf -s %t.rv32 | FileCheck %s --check-prefix=SYM
 
 # RELOCS:      Relocations [
 # RELOCS-NEXT:   Section ({{[0-9]+}}) .rela.dyn {
 # RELOCS-NEXT:     0x12250 R_RISCV_CHERI_RELATIVE - 0x0
 # RELOCS-NEXT:   }
 
+#RELADYN: Contents of section .rela.dyn:
+#RELADYN-NEXT: 10200 
+#RELADYN-NEXT: 10210 00000000 00000000
+
 # GOT: Contents of section .got:
 # GOT-NEXT: 12240 
 # GOT-NEXT: 12250 60320100 00000000 60329904 0070e201
 #                 [    address    ] [      meta     ]
 
-# SYM:        Name: __rela_dyn_start 
-# SYM:        Name: __rela_dyn_end
+# SYM:      0000000000010200    24 NOTYPE  LOCAL  HIDDEN      1 __rela_dyn_start
+# SYM-NEXT: 0000000000010218     0 NOTYPE  LOCAL  HIDDEN      1 __rela_dyn_end
+# SYM:      0000000000013250     4 OBJECT  GLOBAL DEFAULT     4 x
         .text
         .attribute      4, 16
         .attribute      5, "rv64i2p1_zcheripurecap0p9"

lld/test/ELF/cheri/riscv/plt.s
@@ -158,3 +158,4 @@ _start:
 foo:
   ret
 .size foo, . - foo
+

llvm/include/llvm/BinaryFormat/ELFRelocs/RISCV.def
@@ -67,3 +67,4 @@ ELF_RELOC(R_RISCV_CHERI_CJAL,                     199) // Deprecated
 ELF_RELOC(R_RISCV_CHERI_CCALL,                    200) // Deprecated
 ELF_RELOC(R_RISCV_CHERI_RVC_CJUMP,                201) // Deprecated
 ELF_RELOC(R_RISCV_CHERI_RELATIVE,                 202)
+ELF_RELOC(R_RISCV_CHERI_JUMP_SLOT,                203)

jrtc27/cheri-alliance-llvm-project

commit 5c3ce9947e06d0d2aabbbfe3af8409688cb61775

[NFC][ELF][RISCV] Remove unnecessary Cheri.h include
If this were needed, it's not sorted correctly and makes clang-format
unhappy.

Fixes: 93ffe60b8989 ("[CHERI] Support Cheri RISCV Lazy Binding")

lld/ELF/Arch/RISCV.cpp
@@ -17,7 +17,6 @@
 #include "llvm/Support/RISCVAttributes.h"
 #include "llvm/Support/TimeProfiler.h"
 #include "llvm/TargetParser/RISCVISAInfo.h"
-#include "Cheri.h"
 
 using namespace llvm;
 using namespace llvm::object;